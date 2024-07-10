e2aaeb56bba22a8c3e48a2f9de2a16a7dea4e541
Tyrone Francis <tyrone.h.francis@gmail.com>
2024-07-10 10:36:23 +0000

    change debug to false

--- app.py
+++ app.py
@@ -413,4 +413,4 @@ def session_info():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
